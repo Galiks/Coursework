ecb8b9d253946d384ab233158877ccbde61f4984
galiks <Galiks@mail.ru>
2018-03-31 15:09:47 +0400

    easy web-scrap

--- com.turchenkov/Web-scraping.py
+++ com.turchenkov/Web-scraping.py
@@ -10,61 +10,65 @@ from html.parser import HTMLParser
 import re
 
 
-def get_Html(url):
-    try:
-        r = requests.get(url)
-        return r.text
-    except ConnectionError as e:
-        print("Error")
+class Avito:
+
+    def get_Html(self, url):
+        try:
+            r = requests.get(url)
+            return r.text
+        except ConnectionError as e:
+            print("Error")
+
+    def get_All_Links(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        cars = soup.find('div', class_='js-catalog_after-ads').find_all('a', class_='item-description-title-link')
+        links = []
+        for car in cars:
+            link = 'https://www.avito.ru' + car.get('href')
+            links.append(link)
+        return links
+
+    def get_Page_Data(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        try:
+            name = soup.find('span', class_='title-info-title-text').text.strip()
+        except AttributeError as e:
+            name = None
+        try:
+            price = soup.find('span', class_='price-value-string js-price-value-string').text.strip()
+        except AttributeError as e:
+            price = None
+        date = {
+            'name': name[:-6],
+            'price': price[:-2]
+        }
+        return date
+
+    def write_csv(self, data):
+        with open('auto.csv', 'w') as file:
+            writer = csv.writer(file)
+            for line in data:
+                print(line)
+
+                # writer.writerow()
+
+    def main(self):
+        url = 'https://www.avito.ru/saratov/avtomobili/'
+        all_links = self.get_All_Links(self.get_Html(url))
+        for i in all_links:
+            html = self.get_Html(i)
+            data = self.get_Page_Data(html)
+            self.write_csv(data)
 
 
-def get_All_Links(html):
-    soup = BeautifulSoup(html, 'lxml')
-    cars = soup.find('div', class_='js-catalog_after-ads').find_all('a', class_='item-description-title-link')
-    links = []
-    for car in cars:
-        link = 'https://www.avito.ru' + car.get('href')
-        links.append(link)
-    return links
+class Test:
+    html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
+    bs = BeautifulSoup(html, 'lxml')
+    for link in bs.find("div", {"id": "bodyContent"}).find("a", href=re.compile("^(/wiki/)((?!:).)*$")):
+        print(link)
 
 
-def get_Page_Data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    try:
-        name = soup.find('h1', class_='title-info-title-text').text.strip()
-    except AttributeError as e:
-        name = None
+avito = Avito()
+avito.main()
+test = Test()
 
-    try:
-        price = soup.find('span', class_='price-value-string js-price-value-string').text.strip()
-
-    except AttributeError as e:
-        price = None
-    date = {
-        'name': name,
-        'price': price
-    }
-
-    return date
-
-
-def write_csv(data):
-    with open('auto.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow((data['name'],
-                         data['price']))
-        print(data['name'], data['price'])
-
-
-def main():
-    url = 'https://www.avito.ru/saratov/avtomobili/'
-
-    all_links = get_All_Links(get_Html(url))
-    for i in all_links:
-        html = get_Html(i)
-        data = get_Page_Data(html)
-        print(data['name'] + " : " + data['price'])
-        #write_csv(data)
-
-
-main()
