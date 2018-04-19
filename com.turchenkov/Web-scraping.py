723db0e19e9f5b16b9fc33967e28acdd3cf56d47
galiks <Galiks@mail.ru>
2018-04-19 20:48:04 +0400

    second commit

--- com.turchenkov/Web-scraping.py
+++ com.turchenkov/Web-scraping.py
@@ -32,10 +32,12 @@ class Avito:
         soup = BeautifulSoup(html, 'lxml')
         try:
             name = soup.find('span', class_='title-info-title-text').text.strip()
+            print(name)
         except AttributeError as e:
             name = None
         try:
             price = soup.find('span', class_='price-value-string js-price-value-string').text.strip()
+            print(price)
         except AttributeError as e:
             price = None
         date = {
@@ -47,10 +49,8 @@ class Avito:
     def write_csv(self, data):
         with open('auto.csv', 'w') as file:
             writer = csv.writer(file)
-            for line in data:
-                print(line)
 
-                # writer.writerow()
+            # writer.writerow()
 
     def main(self):
         url = 'https://www.avito.ru/saratov/avtomobili/'
@@ -61,14 +61,59 @@ class Avito:
             self.write_csv(data)
 
 
-class Test:
-    html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
-    bs = BeautifulSoup(html, 'lxml')
-    for link in bs.find("div", {"id": "bodyContent"}).find("a", href=re.compile("^(/wiki/)((?!:).)*$")):
-        print(link)
+class Wiki:
+
+    def get_Html(self, url):
+        try:
+            r = requests.get(url)
+            return r.text
+        except ConnectionError as e:
+            print("Error")
+
+    def get_All_Links(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+        cars = soup.find('ul', class_='mw-allpages-chunk').find_all('a')
+        links = []
+        for car in cars:
+            link = 'https://ru.wikipedia.org' + car.get('href')
+            links.append(link)
+        return links
+
+    def get_Page_Data(self, html):
+        soup = BeautifulSoup(html, 'lxml')
+
+        try:
+            birthday = soup.find('span', class_='bday').text.strip()
+            print(birthday)
+        except AttributeError as e:
+            birthday = None
+        try:
+            name = soup.find('h1', class_='firstHeading').text.strip()
+            print(name)
+        except AttributeError as e:
+            name = None
+        date = {
+            'birthday': birthday,
+            'name': name
+        }
+        return date
+
+    def write_csv(self, data):
+        with open('wiki.csv', 'a') as file:
+            writer = csv.writer(file, delimiter=' ')
+            writer.writerow((data['name'], data['birthday']))
+
+    def main(self):
+        url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%92%D1%81%D0%B5_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D1%8B/%D0%A4'
+        all_links = self.get_All_Links(self.get_Html(url))
+        for i in all_links:
+            html = self.get_Html(i)
+            data = self.get_Page_Data(html)
+            self.write_csv(data)
 
 
-avito = Avito()
-avito.main()
-test = Test()
+# avito = Avito()
+# avito.main()
 
+wiki = Wiki()
+wiki.main()
